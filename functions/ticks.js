2af3affd3613a5763b7cd50a169bda053343a3ef
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-26 23:15:52 +1000

    Update ticks.js

--- functions/ticks.js
+++ functions/ticks.js
@@ -9,6 +9,6 @@ execute at @a[tag=ingame] if block ~-1~~ portal run setblock ~-1~~ air destroy
 execute at @a[tag=ingame] if block ~~~-1 portal run title @s actionbar §l§4Notice! §r§cThe nether is disabled.
 execute at @a[tag=ingame] if block ~~~-1 portal run setblock ~~~-1 air destroy
 
-execute at @a[tag=!ingame] if block ~~~ portal run tag add queue
-execute at @a[tag=!ingame,score={}] if block ~~~ portal run scoreboard players add queue pr 1
-execute at @a[tag=!ingame] if block ~~~ portal run scoreboard players set @s pr 1
+# execute at @a[tag=!ingame] if block ~~~ portal run tag add queue
+# execute at @a[tag=!ingame,score={}] if block ~~~ portal run scoreboard players add queue pr 1
+# execute at @a[tag=!ingame] if block ~~~ portal run scoreboard players set @s pr 1
