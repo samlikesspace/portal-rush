7f63a5a81a0f055af4a0abb8332ea100001461b1
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-26 16:02:46 +1000

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,1 +1,26 @@
-
+{
+    "format_version": 2,
+    "header": {
+        "name": "§uPortal §7Rush§r",
+        "description": "§l§6/!\ Warning! §rNothing will work without this pack, do not remove it.",
+        "uuid": "af9ac9cc-0e04-41b7-a16d-10c6eef9b46f",
+        "version": [1, 0, 0],
+        "min_engine_version": [1, 20, 0]
+    },
+    "modules": [
+        {
+            "description": "Script module",
+            "type": "script",
+            "language": "javascript",
+            "uuid": "5d1773f1-4fd7-43ae-97cf-59c7fc36e906",
+            "version": [1, 0, 0],
+            "entry": "scripts/main.js"
+        }
+    ],
+    "dependencies": [
+        {
+            "module_name": "@minecraft/server",
+            "version": "2.7.0-beta"
+        }
+    ]
+}
