f2639a31f385929a32e3eaabcd2223093caf2a14
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-26 21:39:18 +1000

    Update start.js

--- functions/start.js
+++ functions/start.js
@@ -17,7 +17,7 @@ execute at @e[tag=main] run spreadplayers ~ ~ 250 500 @e[type=armor_stand,name="
 execute at @e[tag=main] run structure load portal ~~~
 title @a[tag=queue] title §l§a(!)
 title @a[tag=queue] subtitle §l§2>> §aGame started! §2<<
-title @a[tag=!queue] §l§aNotice! §rA game has just started so there will be a long wait time.
+title @a[tag=!queue] actionbar §l§cNotice! §rA game has just started so there will be a long wait time.
   
 execute at @a[tag=main] run summon armor_stand spawn ~~~
 tag @a remove main
