b70a90a27c848d4e7764d9cf3a359cdee4541fed
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-26 21:03:20 +1000

    Update and rename !manifest.js to omanifest.js

--- omanifest.js
+++ omanifest.js
@@ -2,10 +2,10 @@
 	"format_version": 2,
 	"header": {
 		"name": "§uPortal §7Rush§r",
-        "description": "§l§6/!\ Warning! §rNothing will work without this pack, do not remove it.",
+        "description": "§l§6Warning! §rNothing will work without this pack, do not remove it.",
         "uuid": "d53b4306-adee-4c3b-a9b6-37e5304c3504",
 		"version": [ 1, 0, 0 ],
-		"min_engine_version": [ 1, 21, 100 ]
+		"min_engine_version": [ 1, 21, 80 ]
 	},
 	"modules": [
 		{
