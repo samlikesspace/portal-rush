8d11da3c25d2a3c7424246221332d6f2db5fa185
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-26 23:14:14 +1000

    Create ticks.js

---
+++ functions/ticks.js
@@ -0,0 +1,14 @@
+execute at @a[tag=ingame] if block ~~~ portal run title @s actionbar §l§4Notice! §r§cThe nether is disabled.
+execute at @a[tag=ingame] if block ~~~ portal run setblock ~~~ air destroy
+execute at @a[tag=ingame] if block ~1~~ portal run title @s actionbar §l§4Notice! §r§cThe nether is disabled.
+execute at @a[tag=ingame] if block ~1~~ portal run setblock ~1~~ air destroy
+execute at @a[tag=ingame] if block ~~~1 portal run title @s actionbar §l§4Notice! §r§cThe nether is disabled.
+execute at @a[tag=ingame] if block ~~~1 portal run setblock ~~~1 air destroy
+execute at @a[tag=ingame] if block ~-1~~ portal run title @s actionbar §l§4Notice! §r§cThe nether is disabled.
+execute at @a[tag=ingame] if block ~-1~~ portal run setblock ~-1~~ air destroy
+execute at @a[tag=ingame] if block ~~~-1 portal run title @s actionbar §l§4Notice! §r§cThe nether is disabled.
+execute at @a[tag=ingame] if block ~~~-1 portal run setblock ~~~-1 air destroy
+
+execute at @a[tag=!ingame] if block ~~~ portal run tag add queue
+execute at @a[tag=!ingame,score={}] if block ~~~ portal run scoreboard players add queue pr 1
+execute at @a[tag=!ingame] if block ~~~ portal run scoreboard players set @s pr 1
