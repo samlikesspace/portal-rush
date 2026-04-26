1de3ec925f41afddf755410bb8415efdcd8843f4
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-26 20:40:20 +1000

    Update and rename join.js to start.js

---
+++ functions/start.js
@@ -0,0 +1,17 @@
+summon armor_stand ~~~ portal
+summon armor_stand ~~~ portal
+summon armor_stand ~~~ portal
+summon armor_stand ~~~ portal
+summon armor_stand ~~~ portal
+summon armor_stand ~~~ portal
+summon armor_stand ~~~ portal
+summon armor_stand ~~~ portal
+summon armor_stand ~~~ portal
+summon armor_stand ~~~ portal
+execute at @e[type=armor_stand,name=spawn] run spreadplayers ~ ~ 250 500 @e[name="portal"]
+execute at @e[type=armor_stand,name="portal"] run structure load portal
+title @a title §l§a(!)
+title @a subtitle §l§2>> §aGame started §2<<
+spreadplayers ~ ~ 250 500 @e[tag=queue]
+tag @a[tag=queue] add ingame
+tag @a[tag=playing] remove queue
