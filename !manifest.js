c425dc6577d478428569d897270da65bff970143
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-26 20:47:06 +1000

    Update !manifest.js

--- !manifest.js
+++ !manifest.js
@@ -1,26 +1,17 @@
 {
-    "format_version": 2,
-    "header": {
-        "name": "§uPortal §7Rush§r",
-        "description": "§l§6/!\ Warning! §rNothing will work without this pack, do not remove it.",
-        "uuid": "af9ac9cc-0e04-41b7-a16d-10c6eef9b46f",
-        "version": [1, 0, 0],
-        "min_engine_version": [1, 20, 0]
-    },
-    "modules": [
-        {
-            "description": "Script module",
-            "type": "script",
-            "language": "javascript",
-            "uuid": "5d1773f1-4fd7-43ae-97cf-59c7fc36e906",
-            "version": [1, 0, 0],
-            "entry": "scripts/main.js"
-        }
-    ],
-    "dependencies": [
-        {
-            "module_name": "@minecraft/server",
-            "version": "2.7.0-beta"
-        }
-    ]
+	"format_version": 2,
+	"header": {
+		"name": "What's Different?",
+		"description": "Something is §ooff§r about the world, can you spot what's §bdifferent§r?",
+		"uuid": "d53b4306-adee-4c3b-a9b6-37e5304c3504",
+		"version": [ 1, 0, 0 ],
+		"min_engine_version": [ 1, 21, 100 ]
+	},
+	"modules": [
+		{
+			"type": "data",
+			"uuid": "37bb6867-499f-470f-909c-e044c21f0d90",
+			"version": [ 1, 0, 0 ]
+		}
+	]
 }
