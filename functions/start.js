549d93ffeae3036490f3e6135d2726ca8d582a13
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-26 21:10:40 +1000

    Update start.js

--- functions/start.js
+++ functions/start.js
@@ -9,7 +9,7 @@ summon armor_stand portal ~~~
 summon armor_stand portal ~~~ 
 summon armor_stand portal ~~~
 execute at @e[type=armor_stand,name=spawn] run spreadplayers ~ ~ 250 500 @e[name="portal"]
-execute at @e[type=armor_stand,name="portal"] run structure load portal
+execute at @e[type=armor_stand,name="portal"] run structure load portal ~~~
 title @a title §l§a(!)
 title @a subtitle §l§2>> §aGame started §2<<
 spreadplayers ~ ~ 250 500 @e[tag=queue]
