f174c77ce9a0c40f7c4a44a6d1b05546eed54084
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-26 21:21:19 +1000

    Update start.js

--- functions/start.js
+++ functions/start.js
@@ -15,7 +15,7 @@ spreadplayers ~ ~ 1000 10000 @e[name="spawn"]
 execute at @e[type=armor_stand,name="spawn"] run spreadplayers ~ ~ 250 500 @e[name="portal"]
 execute at @e[type=armor_stand,name="portal"] run structure load portal ~~~
 title @a title §l§a(!)
-title @a subtitle §l§2>> §aGame started §2<<
+title @a subtitle §l§2>> §aGame started! §2<<
 execute at @e[type=armor_stand,name="spawn"] run spreadplayers ~ ~ 250 500 @a[tag=queue]
 tag @a[tag=queue] add ingame
 tag @a[tag=playing] remove queue
