e987f8e4dbad90ef0e44be59c94001326f311090
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-27 13:26:26 +1000

    Update ingame.js

--- functions/ingame.js
+++ functions/ingame.js
@@ -15,9 +15,6 @@ execute as @e[type=armor_stand,name="portal"] if entity @a[r=50] run tag @s add 
 execute as @e[type=armor_stand,name="portal"] if entity @a[r=50] run tag @s add active
 
 
-execute as @p[type=armor_stand,name=portal] at @a[r=50,tag=ingame,hasitem={item=compass,location=slot.weapon.mainhand}] run spawnpoint @s
-
-
 execute as @e[type=armor_stand,name="portal"] run title @a[rm=450] actionbar §l§vWarning! §r§6Your close to the world border!
 execute as @e[type=armor_stand,name="portal"] run title @a[rm=500] actionbar §l§4Notice! §r§cYour outside the world border.
 execute as @e[type=armor_stand,name="portal"] run damage @a[rm=500] 1 void
