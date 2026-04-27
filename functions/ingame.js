4b59c6e79642f08771abf751dfc0414b6825ba47
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-27 11:29:49 +1000

    Update ingame.js

--- functions/ingame.js
+++ functions/ingame.js
@@ -9,6 +9,15 @@ execute at @a[tag=ingame] if block ~-1~~ portal run setblock ~-1~~ air destroy
 execute as @a[tag=ingame] at @s if block ~~~-1 portal run title @s actionbar §l§4Notice! §r§cThe nether is disabled.
 execute at @a[tag=ingame] if block ~~~-1 portal run setblock ~~~-1 air destroy
 
-execute as @a[type=armor_stand,name="portal"] run title @a[rm=450] actionbar §l§vWarning! §r§6Your close to the world border!
-execute as @a[type=armor_stand,name="portal"] run title @a[rm=500] actionbar §l§4Notice! §r§cYour outside the world border.
-execute as @a[type=armor_stand,name="portal"] run damage @a[rm=500] 1 void
+
+execute as @e[type=armor_stand,name="portal"] at @a[r=50] run title @s actionbar §l§2Note! §r§aYour in 50 blocks of an active portal!
+execute as @e[type=armor_stand,name="portal"] if entity @a[r=50] run tag @s add active
+execute as @e[type=armor_stand,name="portal"] if entity @a[r=50] run tag @s add active
+
+
+execute as @a[tag=ingame,hasitem={item=compass,location=slot.weopon.mainhand}] at @p[type=armor_stand,name=portal] run spawnpoint @s
+
+
+execute as @e[type=armor_stand,name="portal"] run title @a[rm=450] actionbar §l§vWarning! §r§6Your close to the world border!
+execute as @e[type=armor_stand,name="portal"] run title @a[rm=500] actionbar §l§4Notice! §r§cYour outside the world border.
+execute as @e[type=armor_stand,name="portal"] run damage @a[rm=500] 1 void
